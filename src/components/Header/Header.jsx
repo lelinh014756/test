eebcb3c3033d35fe67eee161be9c654d36e8d183
Hong Linh <71358321+lelinh014756@users.noreply.github.com>
2022-04-21 16:43:46 +0700

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.scss';
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <ul>
+                <li>Linh dep trai</li>
+                <li>Linh pro</li>
+            </ul>
+        </div>
+    );
+};
+
+export default Header;
